0bd6fc84e486a1d3b284e14f41d4d259db4f486f
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2023-11-02 06:53:39 +0700

    wildan b4b1

---
+++ app/src/main/java/com/example/eleganceathletica/PullUp.java
@@ -0,0 +1,14 @@
+package com.example.eleganceathletica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PullUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pull_up);
+    }
+}
