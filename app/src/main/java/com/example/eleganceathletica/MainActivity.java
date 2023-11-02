073f092e0bb00ee4aa0c32d736a55bd6c14242fe
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2023-11-02 15:48:44 +0700

    update and add times

--- app/src/main/java/com/example/eleganceathletica/MainActivity.java
+++ app/src/main/java/com/example/eleganceathletica/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
         pushUp = findViewById(R.id.buttonPushUp);
         pullUp = findViewById(R.id.buttonPullUp);
+        sitUp = findViewById(R.id.buttonSitUp);
+        handstand = findViewById(R.id.buttonHandstand);
+        jump = findViewById(R.id.buttonJump);
+        jumpRope = findViewById(R.id.buttonJumpRope);
+        bicepsCurl = findViewById(R.id.buttonBicepsCurl);
 
         pushUp.setOnClickListener(V -> {
             startActivity(new Intent(getApplicationContext(), PushUp.class));
@@ -23,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
         pullUp.setOnClickListener(V -> {
             startActivity(new Intent(getApplicationContext(), PullUp.class));
+        });
+
+        sitUp.setOnClickListener(V -> {
+            startActivity(new Intent(getApplicationContext(), SitUp.class));
+        });
+
+        handstand.setOnClickListener(V -> {
+            startActivity(new Intent(getApplicationContext(), Handstand.class));
+        });
+
+        jumpRope.setOnClickListener(V -> {
+            startActivity(new Intent(getApplicationContext(), JumpRope.class));
+        });
+
+        bicepsCurl.setOnClickListener(V -> {
+            startActivity(new Intent(getApplicationContext(), BicepsCurl.class));
         });
     }
 }
--- app/src/main/java/com/example/eleganceathletica/PullUp.java
+++ app/src/main/java/com/example/eleganceathletica/PullUp.java
@@ -2,13 +2,22 @@ package com.example.eleganceathletica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PullUp extends AppCompatActivity {
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pull_up);
+
+        back = findViewById(R.id.buttonBack1);
+
+        back.setOnClickListener(V -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        });
     }
 }
