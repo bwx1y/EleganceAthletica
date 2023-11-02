a91dfc160b3b4d0259e98d2f627aa8206fcfe4cc
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2023-11-02 17:09:19 +0700

    set default times in pull up

--- app/src/main/java/com/example/eleganceathletica/PullUp.java
+++ app/src/main/java/com/example/eleganceathletica/PullUp.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class PullUp extends AppCompatActivity {
     ImageView back;
+
+    Button start, youtube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +18,19 @@ public class PullUp extends AppCompatActivity {
         setContentView(R.layout.activity_pull_up);
 
         back = findViewById(R.id.buttonBack1);
+        start = findViewById(R.id.buttonStartPullUp);
+        youtube = findViewById(R.id.buttonYoutubePullUp);
 
         back.setOnClickListener(V -> {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
         });
+
+        start.setOnClickListener(V -> {
+            String times = "50000"; // satuan times adalah milisecond
+            Intent intent = new Intent(PullUp.this, Times.class);
+            intent.putExtra("data", times);
+            startActivity(intent);
+        });
+
     }
 }
--- app/src/main/java/com/example/eleganceathletica/Times.java
+++ app/src/main/java/com/example/eleganceathletica/Times.java
@@ -3,12 +3,21 @@ package com.example.eleganceathletica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Times extends AppCompatActivity {
 
+    TextView viewTimes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_times);
+
+        viewTimes = findViewById(R.id.textViewtes);
+        int times = Integer.parseInt(getIntent().getStringExtra("data"));
+
+        if (getIntent().getExtras() != null) {
+            viewTimes.setText(getIntent().getStringExtra("data"));
+        }
     }
 }
