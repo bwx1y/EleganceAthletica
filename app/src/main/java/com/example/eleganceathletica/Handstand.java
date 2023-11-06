aea69f9322196d8987a999dfb871d61641b326d7
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2023-11-06 11:35:37 +0700

    update

--- app/src/main/java/com/example/eleganceathletica/Handstand.java
+++ app/src/main/java/com/example/eleganceathletica/Handstand.java
@@ -2,13 +2,29 @@ package com.example.eleganceathletica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Handstand extends AppCompatActivity {
+    private final String urlYoutube = "https://youtube.com/"; // link untuk video youtube
+    private final long times = 600000; // satuan times adalah milisecond
+    private ImageView back;
+    private Button start, youtube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handstand);
+
+        this.back = findViewById(R.id.buttonBack);
+        this.start = findViewById(R.id.buttonStartPullUp);
+        this.youtube = findViewById(R.id.buttonYoutubePullUp);
+
+        this.back.setOnClickListener(V -> startActivity(new Intent(getApplicationContext(), MainActivity.class)));
+        this.youtube.setOnClickListener(V -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(this.urlYoutube))));
+        this.start.setOnClickListener(V -> startActivity(new Intent(Handstand.this, Times.class).putExtra("times", this.times)));
     }
 }
