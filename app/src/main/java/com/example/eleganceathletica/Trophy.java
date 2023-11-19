38d14d1a065621fa3f3fed4da0cccf23361986f1
Nedy Nugroho <87101532+bwx1y@users.noreply.github.com>
2023-11-19 15:58:48 +0700

    Finally

--- app/src/main/java/com/example/eleganceathletica/Trophy.java
+++ app/src/main/java/com/example/eleganceathletica/Trophy.java
@@ -2,13 +2,19 @@ package com.example.eleganceathletica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Trophy extends AppCompatActivity {
+    private ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy);
+
+        this.back = findViewById(R.id.imageView2);
+        this.back.setOnClickListener(V -> startActivity(new Intent(Trophy.this, MainActivity.class)));
     }
 }
